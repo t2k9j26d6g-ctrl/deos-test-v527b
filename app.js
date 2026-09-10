2a2bb3210d965c770df309749bee5223841bce20
t2k9j26d6g-ctrl <t2k9j26d6g@privaterelay.appleid.com>
2026-09-10 11:34:55 +0200

    Update app.js
    P2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const DEOS_VERSION = "V5.30P1";
+const DEOS_VERSION = "V5.30P2";
 
 // -- V5.23C : feedback visuel commun pour les actions asynchrones ----------------
 function ensureDeosAsyncFeedbackUi() {
